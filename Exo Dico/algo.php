2f14bba59ace3314793d16c200f6b77863ba1504
moonye@live.fr <moonye@live.fr>
2024-12-04 16:05:59 +0100

    EXO 1 et un peu du 2

---
+++ Exo Dico/algo.php
@@ -0,0 +1,28 @@
+<?php
+// EXERCICE 1
+echo "Exercice 1";
+echo "<br>";
+$string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
+$dico = explode("\n", $string);
+
+
+echo "Ce dico contient un total de " . (count($dico)) . " mots.";
+
+
+// EXERCICE 1
+echo "<br>";
+echo "<br>";
+echo "Exercice 2";
+echo "<br>";
+
+$quinzecharaTABLE = [];
+$quinzechara = strlen($dico);
+
+for($dico =0; $dico < count($dico) ; $i+=1){
+if($dico.$value === ){
+
+}}
+
+echo "Ce dico contient un total de " . (count($quinzecharaTABLE)) . " mots faisant exactment 15 catactères.";
+
+var_dump($dico);
